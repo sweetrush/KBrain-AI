7aab9e11c30fd56796d561b3fea8da7218fe5020
Suetena <suetena_coder@outlook.com>
2024-07-01 10:15:18 +1300

    Update run_miah_stGemini.sh

--- run_miah_stGemini.sh
+++ run_miah_stGemini.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 clear
-echo '.';
-echo '.';
-echo '. [shell-info] Runing StreamIT Miah Gemini AI';
-echo '. [shell-info] Change Director to AI Directory';
+echo '  ';
+echo '  ';
+echo '    [shell-info] Runing StreamIT Miah Gemini AI';
+echo '    [shell-info] Change Director to AI Directory';
 cd /home/syr/Development/GithubDevs/KBrain-AI/
-echo '. [App-info] Streamlit Loading the ai';
+echo '    [App-info] Streamlit Loading the ai';
 streamlit run Main.py
