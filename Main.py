2ab4a933891a7f443bdc42163a95b0be578b7ce2
Suetena <suetena_coder@outlook.com>
2024-12-25 12:14:02 +0000

    ggg

--- Main.py
+++ Main.py
@@ -1019,6 +1019,7 @@ def upload_to_gemini(path, mime_type=None):
     """
     file = genai.upload_file(path, mime_type=mime_type)
     print(f"Uploaded file '{file.display_name}' as: {file.uri}")
+    st.write(file)
     return file
 
 #########################################################################
