285bb2359ce0f46cc5955c868f46fd6d76b664c8
Suetena <suetena_coder@outlook.com>
2024-12-28 00:32:50 +0000

    ddsssaasde

--- Main.py
+++ Main.py
@@ -1142,14 +1142,14 @@ model_tokens = "8024"
 
 
 models = [
-    "gemini-2.0-flash-thinking-exp"
-    "gemini-2.0-flash-thinking-exp-1219"
+    "gemini-2.0-flash-thinking-exp",
+    "gemini-2.0-flash-thinking-exp-1219",
     "gemini-2.0-flash-exp",
     "gemini-1.5-pro-latest",
     "gemini-1.5-pro-002",
     "gemini-1.5-pro-exp-0827",
     "gemini-exp-1206",
-    "gemini-1.5-flash-latest",
+    "gemini-1.5-flash-latest"
 ]
 
 # OLD Definition
